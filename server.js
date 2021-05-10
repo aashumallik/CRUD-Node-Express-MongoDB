f9d605439a2eb9253d4905ae9a35f163acf65eff
aashumallik <amallik@>
2021-05-09 19:06:27 -0500

    setup server.js

--- server.js
+++ server.js
@@ -1,1 +1,50 @@
  //starting the server
+ // installing morgan to help a log message everytime we make a request
+ // installing nodemon to start server automatically
+ // installing ejs for creating dynamic HTML
+ // installing body-parser to serialize data
+ // installing dotenv to separate source code to hide sensitive info
+ // installing mongoose to connect to mongo db
+// installing axios makes it easy to make request express application
+
+//creating asset folder - all the asset for the proj
+//creating views for html files
+
+//creating my HTTP server
+
+
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+
+const connectDB = require('./server/database/connection');
+
+const app = express();
+
+dotenv.config( { path : 'config.env'} )
+const PORT = process.env.PORT || 8080
+
+// log requests
+app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
+
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+// load routers
+app.use('/', require('./server/routes/router'))
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
